f303e557cbf5c6ca08f8026dddb5374c2a0540be
Marius Reimer <reime005@gmail.com>
2019-06-11 23:48:46 +0200

    added android mask implementation

---
+++ android/src/main/java/org/reactnative/maskedview/RNCMaskedViewManager.java
@@ -0,0 +1,31 @@
+package org.reactnative.maskedview;
+
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class RNCMaskedViewManager extends ViewGroupManager<RNCMaskedView> {
+  private static final String REACT_CLASS = "RNCMaskedView";
+
+  @Override
+  public String getName() {
+    return REACT_CLASS;
+  }
+
+  @Override
+  protected RNCMaskedView createViewInstance(ThemedReactContext themedReactContext) {
+    return new RNCMaskedView(themedReactContext);
+  }
+}
