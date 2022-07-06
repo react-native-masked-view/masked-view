04574d186b5bcc0f658c8b7094f2f93400bc89d2
Luis Fernando <luis.betancourt@ank.app>
2022-07-05 21:30:49 -0300

    chore: fix import newArch

--- android/src/newarch/java/org/reactnative/maskedview/RNCMaskedViewManager.java
+++ android/src/newarch/java/org/reactnative/maskedview/RNCMaskedViewManager.java
@@ -6,7 +6,9 @@ import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.common.MapBuilder;
+import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
