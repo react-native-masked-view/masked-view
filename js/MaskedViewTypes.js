46c6c1c039020fbc513e17560d6f70f490e4d024
Jesse Katsumata <jesse.katsumata@gmail.com>
2024-11-04 11:51:26 +0900

    fix: lint error (#231)
    * fix: lint error
    
    * Fix: flow error

--- js/MaskedViewTypes.js
+++ js/MaskedViewTypes.js
@@ -1,5 +1,5 @@
 // @flow
-import { type Node, type Element } from 'react';
+import { type Node } from 'react';
 import { type ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
 
 export type MaskedViewProps = Partial<ViewProps> &
@@ -9,7 +9,7 @@ export type MaskedViewProps = Partial<ViewProps> &
      * Should be a React element to be rendered and applied as the
      * mask for the child element.
      */
-    maskElement: Element<any>,
+    maskElement: Node,
     /**
      * Opt into software rendering to enable animated masks.
      */
